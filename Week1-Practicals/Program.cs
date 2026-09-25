9d9d9b4db9a30b0c362f5cf347faa466c9eb85cb
c-H097 <meriemh706@gmail.com>
2026-09-25 16:07:46 +0300

    od

--- Week1-Practicals/Program.cs
+++ Week1-Practicals/Program.cs
@@ -10,7 +10,7 @@
 
 
 // Task 2: Selection statements
-// use IF-ELSE, reads user age and determines wethere they are a child, teen or adult
+// use IF-ELSE, reads user age and determines whether they are a child, teen or adult
 // display result to console
 // Ensures that the program handles invalid input 
 
