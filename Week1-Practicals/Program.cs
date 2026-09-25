0db77774eb9972d632b5f699fd785883decd89f0
c-H097 <meriemh706@gmail.com>
2026-09-25 15:41:45 +0300

    updated

--- Week1-Practicals/Program.cs
+++ Week1-Practicals/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+ * Week 1 Practicals
+ * Task 1
+ * Task 2
+ * Task 3
+ * Task 4
+ * Task 5
+*/
+
+
+// Task 1: Processing user input
+// asks user for their name and age, displays name and age after 5 years
+
+
+
+// Task 2: Selection statements
+// use IF-ELSE, reads user age and determines wethere they are a child, teen or adult
+// display result to console
+// Ensures that the program handles invalid input 
+
+
+// Task 3: Logical operators
+// use IF-ELSE, reads user integer input and determines if number is positive, negative or zero
+// prints the result to console
+
+
+
+// Task 4: Leap year checker
+// 
