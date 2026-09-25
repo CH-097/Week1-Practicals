eee5b8528a6ed54c548310effc8b6d4eb9a6ddc2
c-H097 <meriemh706@gmail.com>
2026-09-25 16:04:43 +0300

    upd

--- Week1-Practicals/Task2.cs
+++ Week1-Practicals/Task2.cs
@@ -1,6 +1,10 @@
 ﻿// Task 2: Selection statements
-// use IF-ELSE, reads user age and determines wethere they are a child, teen or adult
+// use IF-ELSE, reads user age and determines whether they are a child, teen or adult
 // display result to console
 // Ensures that the program handles invalid input 
+
+
+Console.WriteLine("Enter your age:");
+int age = Convert.ToInt32(Console.ReadLine());
 
 
